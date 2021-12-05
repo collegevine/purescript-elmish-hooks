3de39d1ea604933f2756080e7f7f65ae04238d9f
Marc Cordova <mcordova47@gmail.com>
2021-12-04 23:55:00 -0500

    Fix useEffect

--- src/Elmish/Hooks/UseEffect.purs
+++ src/Elmish/Hooks/UseEffect.purs
@@ -5,12 +5,12 @@ module Elmish.Hooks.UseEffect
 import Prelude
 
 import Effect.Aff (Aff)
-import Elmish (ReactElement, fork)
+import Elmish (ReactElement, forkVoid)
 import Elmish.Component (ComponentName, wrapWithLocalState)
 
 useEffect :: ComponentName -> { init :: Aff Unit, render :: Unit -> ReactElement } -> ReactElement
 useEffect name = wrapWithLocalState name \{ init, render } ->
-  { init: fork init
+  { init: forkVoid init
   , update: \_ _ -> pure unit
   , view: \_ _ -> render unit
   }
