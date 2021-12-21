35c277c0b4b001aa3a22a4e96fa5fe973d4143cd
Marc Cordova <marc.cordova@collegevine.com>
2021-12-21 08:59:24 -0500

    Review feedback (#2)

--- src/Elmish/Hooks/UseState.purs
+++ src/Elmish/Hooks/UseState.purs
@@ -7,24 +7,28 @@ import Prelude
 
 import Data.Tuple (curry)
 import Data.Tuple.Nested (type (/\))
-import Elmish (Dispatch, ReactElement)
-import Elmish.Component (ComponentName, wrapWithLocalState)
+import Elmish (Dispatch)
 import Elmish.Hooks.Type (Hook, HookName, mkHook)
 
-type Args state =
-  { initialState :: state
-  , render :: state /\ Dispatch state -> ReactElement
-  }
-
-useState' :: forall state. ComponentName -> Args state -> ReactElement
-useState' name = wrapWithLocalState name \{ initialState, render } ->
-  { init: pure initialState
-  , update: \_ newState -> pure newState
-  , view: curry render
-  }
-
--- | The `useState` hook takes an initial state and its callback has access to
--- | the current state and a setter for the state.
+-- | The `useState` hook takes an initial state and returns a `Hook`
+-- | encapsulating the current state and a `setState` function. E.g.:
+-- |
+-- | ```purs
+-- | view :: ReactElement
+-- | view = withHooks do
+-- |   visible /\ setVisible <- useState (HookName "ContentVisible") false
+-- |   pure $
+-- |     H.fragment
+-- |     [ H.button_ "" { onClick: setVisible $ not visible } "Toggle visibility"
+-- |     , if visible
+-- |         then H.div "" "Content"
+-- |         else H.empty
+-- |     ]
+-- | ```
 useState :: forall state. HookName -> state -> Hook (state /\ Dispatch state)
 useState name initialState =
-  mkHook name \n render -> useState' n { initialState, render }
+  mkHook name \render ->
+    { init: pure initialState
+    , update: \_ newState -> pure newState
+    , view: curry render
+    }
