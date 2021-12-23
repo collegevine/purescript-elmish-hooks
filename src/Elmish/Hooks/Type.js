52dfdce3ac163a8716f8114af5fe933115ba5c6d
Marc Cordova <mcordova47@gmail.com>
2021-12-23 01:36:53 -0500

    Only use hook call site for uuid

--- src/Elmish/Hooks/Type.js
+++ src/Elmish/Hooks/Type.js
@@ -1,3 +1,8 @@
 const uuidV5 = require('uuid/v5')
 
-exports.genStableUUID = () => uuidV5(new Error().stack, '31877c6f-998d-44e6-99e6-3cd31a643f1d')
+exports.genStableUUID = () => {
+  const stack = new Error().stack
+  const stackLines = stack.split('\n')
+  const hookCallSite = stackLines[stackLines.length - 3] // TODO: Add tests to ensure this stays correct
+  return uuidV5(hookCallSite, '31877c6f-998d-44e6-99e6-3cd31a643f1d')
+}
