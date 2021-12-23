bff81e8b384710f5e808c743d8cde16712b7bd8b
Marc Cordova <mcordova47@gmail.com>
2021-12-23 01:38:01 -0500

    Add link to PR comment

--- src/Elmish/Hooks/Type.js
+++ src/Elmish/Hooks/Type.js
@@ -3,6 +3,9 @@ const uuidV5 = require('uuid/v5')
 exports.genStableUUID = () => {
   const stack = new Error().stack
   const stackLines = stack.split('\n')
-  const hookCallSite = stackLines[stackLines.length - 3] // TODO: Add tests to ensure this stays correct
+  // TODO: Add tests to ensure this stays correct: See
+  // https://github.com/collegevine/purescript-elmish-hooks/pull/3#discussion_r774324284
+  // for an example.
+  const hookCallSite = stackLines[stackLines.length - 3]
   return uuidV5(hookCallSite, '31877c6f-998d-44e6-99e6-3cd31a643f1d')
 }
