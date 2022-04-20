a453ed1d63ad065ab0d27e67730db6d4df06ea2d
Fyodor Soikin <name.fa@gmail.com>
2022-04-20 17:27:58 +0200

    Upgrade Elmish to v0.7.0 (#20)

--- src/Elmish/Hooks/UseEffect.purs
+++ src/Elmish/Hooks/UseEffect.purs
@@ -14,8 +14,8 @@ import Effect.Aff (Aff)
 import Elmish (EffectFn1, ComponentDef, createElement, forkVoid, withTrace, (<?|))
 import Elmish.Component (ComponentName(..))
 import Elmish.Hooks.Type (Hook, HookType, mkHook)
-import Elmish.React.Import (EmptyProps, ImportedReactComponent, ImportedReactComponentConstructorWithContent)
-import Elmish.Ref (Ref, deref, ref)
+import Elmish.React.Import (ImportedReactComponent, ImportedReactComponentConstructorWithContent)
+import Elmish.Opaque (Opaque, unwrap, wrap) as Opaque
 
 foreign import data UseEffect :: Type -> HookType
 
@@ -80,22 +80,21 @@ useEffect_ name f deps runEffect =
     , view: \_ dispatch ->
         useEffectLifeCycles
           { componentDidUpdate: dispatch <?| \prevDeps ->
-              if deref prevDeps /= deps then
+              if Opaque.unwrap prevDeps /= deps then
                 Just deps
               else
                 Nothing
-          , deps: ref deps
+          , deps: Opaque.wrap deps
           } $
           render unit
     }
 
-type Props deps r =
-  ( componentDidUpdate :: EffectFn1 (Ref "deps" deps) Unit
-  , deps :: Ref "deps" deps
-  | r
+type Props deps =
+  ( componentDidUpdate :: EffectFn1 (Opaque.Opaque "deps" deps) Unit
+  , deps :: Opaque.Opaque "deps" deps
   )
 
-useEffectLifeCycles :: forall deps. ImportedReactComponentConstructorWithContent (Props deps) EmptyProps
+useEffectLifeCycles :: forall deps. ImportedReactComponentConstructorWithContent (Props deps)
 useEffectLifeCycles = createElement useEffectLifeCycles_
 
 foreign import useEffectLifeCycles_ :: ImportedReactComponent
