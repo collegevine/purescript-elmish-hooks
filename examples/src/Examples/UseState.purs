bb67398ad8df2f3c84c37a8e70cc82b0f54939b0
Fyodor Soikin <name.fa@gmail.com>
2022-12-19 15:27:57 -0500

    Fix examples

--- examples/src/Examples/UseState.purs
+++ examples/src/Examples/UseState.purs
@@ -4,7 +4,7 @@ module Examples.UseState
 
 import Prelude
 
-import Elmish (ReactElement)
+import Elmish (ReactElement, (<|))
 import Elmish.HTML.Styled as H
 import Elmish.Hooks (useState)
 import Elmish.Hooks.Type ((=/>))
@@ -16,7 +16,7 @@ view = useState false =/> \visible setVisible ->
     [ H.code "" "useState"
     , H.text " hook"
     ]
-  , H.button_ "btn btn-primary" { onClick: setVisible $ not visible } "Show"
+  , H.button_ "btn btn-primary" { onClick: setVisible <| not visible } "Show"
   , if visible then
       H.fragment
       [ H.div_ "modal fade show d-block" { style: H.css { pointerEvents: "none" } } $
@@ -27,11 +27,11 @@ view = useState false =/> \visible setVisible ->
                 [ H.div "col" $
                     H.h3 "header-title" "Modal"
                 , H.div "col-auto" $
-                    H.button_ "btn btn-icon btn-lg p-0 text-muted" { onClick: setVisible false } "×"
+                    H.button_ "btn btn-icon btn-lg p-0 text-muted" { onClick: setVisible <| false } "×"
                 ]
               , H.div "py-4" "Content"
               ]
-      , H.div_ "modal-backdrop fade show" { onClick: setVisible false } H.empty
+      , H.div_ "modal-backdrop fade show" { onClick: setVisible <| false } H.empty
       ]
     else
       H.empty
